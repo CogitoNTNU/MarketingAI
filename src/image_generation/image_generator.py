0935f42785319db845efaf70329f783bb71bdf63
Olav <olavslo@stud.ntnu.no>
2023-09-12 18:31:06 +0200

    feat: Wrote tests for image generation

--- src/image_generation/image_generator.py
+++ src/image_generation/image_generator.py
@@ -5,18 +5,28 @@ import openai
 import os
 
 class ImageGenerator:
-    def get_user_prompt():
+    MAX_PROMPT_LENGTH = 1000
+
+    def get_user_prompt(self):
         image_prompt = input('What do you want to generate an image of? ')
         
+        self.validate_prompt(image_prompt)
+
+        return image_prompt
+
+    def validate_prompt(self, image_prompt: str):
         if not isinstance(image_prompt, str):
             raise TypeError('Prompt must be a string')
         
         if len(image_prompt) == 0:
             raise ValueError('Prompt must not be empty')
+        
+        if (len(image_prompt) > self.MAX_PROMPT_LENGTH):
+            raise ValueError('Prompt must be less than 1000 characters')
 
-        return image_prompt
+    def generate_image(self, image_prompt: str):
+        self.validate_prompt(image_prompt)
 
-    def generate_image(image_prompt: str):
         openai.api_key = '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' # TODO: Make sure this is the way we get environment variables
         response = openai.Image.create(
             prompt=image_prompt,
--- tests/image_generation/image_generator_test.py
+++ tests/image_generation/image_generator_test.py
@@ -2,20 +2,53 @@ import pytest
 
 from src.image_generation.image_generator import ImageGenerator
 
-
-
-
-
-def test_image_generation_with_empty_prompt():
+def test_image_generation_with_empty_string_prompt():
     # Arrange
 
     image_generator = ImageGenerator()
     no_prompt = ""
-    pytest.raises(ValueError):
+    with pytest.raises(ValueError):
         # Act
         result = image_generator.generate_image(no_prompt)
         # Assert
-    
 
+def test_image_generation_with_non_string_prompt():
+    # Arrange
 
-    
+    image_generator = ImageGenerator()
+    non_string_prompt = 1
+    with pytest.raises(TypeError):
+        # Act
+        result = image_generator.generate_image(non_string_prompt)
+        # Assert
+
+def test_image_generation_with_none_prompt():
+    # Arrange
+
+    image_generator = ImageGenerator()
+    none_prompt = None
+    with pytest.raises(TypeError):
+        # Act
+        result = image_generator.generate_image(none_prompt)
+        # Assert
+
+def test_image_generation_with_too_long_prompt():
+    # Arrange
+
+    image_generator = ImageGenerator()
+    too_long_prompt = "A" * 3001
+    with pytest.raises(ValueError):
+        # Act
+        result = image_generator.generate_image(too_long_prompt)
+        # Assert
+
+@pytest.mark.apitest
+def test_image_generation_with_valid_prompt():
+    # Arrange
+
+    image_generator = ImageGenerator()
+    valid_prompt = "A picture of a cat"
+    # Act
+    result = image_generator.generate_image(valid_prompt)
+    # Assert
+    assert result is not None
