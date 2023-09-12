1c803d1a7dff69528ecfbedd46bd9f4b6e613759
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-09-12 23:22:29 +0200

    Refactor: Make private methods start with underscore

--- src/image_generation/image_generator.py
+++ src/image_generation/image_generator.py
@@ -9,10 +9,10 @@ class ImageGenerator:
 
     def get_user_prompt(self) -> str:
         image_prompt = input('What do you want to generate an image of? ')
-        self.validate_prompt(image_prompt)
+        self._validate_prompt(image_prompt)
         return image_prompt
 
-    def validate_prompt(self, image_prompt: str) -> None:
+    def _validate_prompt(self, image_prompt: str) -> None:
         if not isinstance(image_prompt, str):
             raise TypeError('Prompt must be a string')
         
@@ -22,7 +22,7 @@ class ImageGenerator:
         if (len(image_prompt) > self.MAX_PROMPT_LENGTH):
             raise ValueError(f'Prompt must be less than {self.MAX_PROMPT_LENGTH} characters')
 
-    def validate_size(self, width: int, height: int) -> None:
+    def _validate_size(self, width: int, height: int) -> None:
         if not isinstance(width, int):
             raise TypeError('Width must be an integer')
         
@@ -45,8 +45,8 @@ class ImageGenerator:
         Returns:
             A dictionary containing the image url
         '''
-        self.validate_prompt(image_prompt)
-        self.validate_size(width, height)
+        self._validate_prompt(image_prompt)
+        self._validate_size(width, height)
         size = str(width) + 'x' + str(height)
         openai.api_key = '' # TODO: Make sure this is the way we get environment variables
         response = openai.Image.create(
