97e73c61ee08dced39a4a450c8826f3787414568
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-09-19 18:26:10 +0200

    feat: :sparkles: Completed the connection to GPT
    Completed the connection to the GPT API with the function request_chat_completion
    
    #8 @HFossdal

--- src/gpt/gpt_api.py
+++ src/gpt/gpt_api.py
@@ -1,10 +1,12 @@
 import os
 import openai
-import config
+from openai.error import OpenAIError
+from src.config import Config
+
 
 try:
     # Set OpenAI API key
-    api_key = config.api_key
+    api_key = Config().API_KEY
     openai.api_key = api_key
 except:
     print("OpenAI API key not found. Please set the environment variable OPENAI_API_KEY to your API key.")
@@ -13,26 +15,32 @@ except:
 
 def request_chat_completion(previous_message:dict, role:str = "system",message :str = ""):
 
-    if(not (role == "system" or "user" or "assistant")):
-        print("Invalid role")
+    try:
+        if(not (role == "system" or "user" or "assistant")):
+            print("Invalid role")
+            return None
+        
+        if(previous_message):
+            response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                previous_message,
+                {"role": role, "content": message}
+            ]
+            )
+        else: 
+            response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": role, "content": message}, 
+            ]
+            )
+        print(response)
+        return response
+    except OpenAIError as error:
+        print(f"An error has occured while requesting chat completion.")
+        print(f"The error: {str(error)}")
         return None
-    
-    if(previous_message):
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            previous_message,
-            {"role": role, "content": message}
-        ]
-        )
-    else: 
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": role, "content": message}, 
-        ]
-        )
-
-    print(response)
-
-request_chat_completion(None,"system", "Cogito is better then Brain")
+    except Exception as e: 
+        print(f"An unexpected error occured: {str(e)}")
+        return None
