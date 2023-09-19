0f0dcf646acb0701811baea6c6b12255fce50a40
Joergen Jore <ntnu@berkeley.edu>
2023-09-19 19:43:16 +0200

    Integarted gtp in main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.image_generation.image_generator import ImageGenerator
-
+from src.gpt.gpt_api import request_chat_completion
 import logging
 
 # Set up logging
@@ -10,8 +10,12 @@ logging.basicConfig(filename='PropagandaAI.log',
 logger = logging.getLogger(__name__)
 
 logger.info('Starting PropagandaAI')
+prompt: str = input('What shall propogandaAI generate: ')
+result: str = request_chat_completion(None, 'system', prompt)["choices"][0]["message"]["content"]
+
+logger.info('Generating Text on prompt')
+logger.info(f'Starting image generation based on prompt: {result}')
 
 image_generator = ImageGenerator()
-image_prompt = image_generator.get_user_prompt()
-image = image_generator.generate_image(image_prompt, 512, 512)
+image = image_generator.generate_image(result, 512, 512)
 print(image)
