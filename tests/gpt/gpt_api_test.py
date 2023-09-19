5108b4872612cd1f73ee66769dd20d7c840f8f93
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-09-19 18:43:41 +0200

    test: :white_check_mark: Refactor and add tests for GPT_API
    Added more tests and refactored the ones that were there with the @pytest.mark.apitest
    
    #8 @HFossdal

--- tests/gpt/gpt_api_test.py
+++ tests/gpt/gpt_api_test.py
@@ -1,13 +1,50 @@
 from src.gpt.gpt_api import *
+import pytest
 
-def test_request_chat_completion():
-    assert request_chat_completion(None, "system", "Hello, how are you?") is not None
-    assert request_chat_completion(None, "user", "I am fine, how are you?") is not None
-    assert request_chat_completion(None, "assistant", "I am fine, how are you?") is not None
+@pytest.mark.apitest
+def test_request_chat_completion_as_system():
+    # Act
+    result = request_chat_completion(None, "system", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
 
-def test_with_previous_message():
+@pytest.mark.apitest
+def test_request_chat_completion_as_user():
+    # Act
+    result = request_chat_completion(None, "user", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
+
+@pytest.mark.apitest
+def test_request_chat_completion_as_assistant():
+    # Act
+    result = request_chat_completion(None, "assistant", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
+
+@pytest.mark.apitest
+def test_with_previous_message_as_system():
+    # Arrange
     previous_message = {"role": "system", "content": "Hello, how are you?"}
-    assert request_chat_completion(previous_message, "user", "I am fine, how are you?") is not None
-    assert request_chat_completion(previous_message, "assistant", "I am fine, how are you?") is not None
+    # Act
+    result = request_chat_completion(previous_message, "system", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
 
+@pytest.mark.apitest
+def test_with_previous_message_as_user():
+    # Arrange
+    previous_message = {"role": "system", "content": "Hello, how are you?"}
+    # Act
+    result = request_chat_completion(previous_message, "user", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
 
+@pytest.mark.apitest
+def test_with_previous_message_as_assistant():
+    # Arrange
+    previous_message = {"role": "system", "content": "Hello, how are you?"}
+    # Act
+    result = request_chat_completion(previous_message, "assistant", "I am fine, how are you?")
+    # Assert
+    assert isinstance(result,str), "Result is not a string"
