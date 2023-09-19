ae216501b7941d4640516c45b7aba216887626fa
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-09-19 18:57:12 +0200

    refactor: :recycle: Refactored the variable for result
    Refactored the variable for result to get the message as a string
    
    #8 @HFossdal

--- tests/gpt/gpt_api_test.py
+++ tests/gpt/gpt_api_test.py
@@ -4,21 +4,21 @@ import pytest
 @pytest.mark.apitest
 def test_request_chat_completion_as_system():
     # Act
-    result = request_chat_completion(None, "system", "I am fine, how are you?")
+    result = request_chat_completion(None, "system", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
 
 @pytest.mark.apitest
 def test_request_chat_completion_as_user():
     # Act
-    result = request_chat_completion(None, "user", "I am fine, how are you?")
+    result = request_chat_completion(None, "user", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
 
 @pytest.mark.apitest
 def test_request_chat_completion_as_assistant():
     # Act
-    result = request_chat_completion(None, "assistant", "I am fine, how are you?")
+    result = request_chat_completion(None, "assistant", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
 
@@ -27,7 +27,7 @@ def test_with_previous_message_as_system():
     # Arrange
     previous_message = {"role": "system", "content": "Hello, how are you?"}
     # Act
-    result = request_chat_completion(previous_message, "system", "I am fine, how are you?")
+    result = request_chat_completion(previous_message, "system", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
 
@@ -36,7 +36,7 @@ def test_with_previous_message_as_user():
     # Arrange
     previous_message = {"role": "system", "content": "Hello, how are you?"}
     # Act
-    result = request_chat_completion(previous_message, "user", "I am fine, how are you?")
+    result = request_chat_completion(previous_message, "user", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
 
@@ -45,6 +45,6 @@ def test_with_previous_message_as_assistant():
     # Arrange
     previous_message = {"role": "system", "content": "Hello, how are you?"}
     # Act
-    result = request_chat_completion(previous_message, "assistant", "I am fine, how are you?")
+    result = request_chat_completion(previous_message, "assistant", "I am fine, how are you?")["choices"][0]["message"]["content"]
     # Assert
     assert isinstance(result,str), "Result is not a string"
