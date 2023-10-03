a8d91367b424fde743a1716e549a2fbd471121ed
Sverre Nystad <sverrenystad@gmail.com>
2023-10-04 01:08:14 +0200

    Feat: Integrate image saving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from src.image_generation.image_generator import ImageGenerator
+from src.image_generation.image_generator import ImageGenerator, download_and_save_image
 from src.gpt.gpt_api import request_chat_completion
 import logging
+
+
 
 # Set up logging
 logging.basicConfig(filename='PropagandaAI.log',
@@ -10,12 +12,17 @@ logging.basicConfig(filename='PropagandaAI.log',
 logger = logging.getLogger(__name__)
 
 logger.info('Starting PropagandaAI')
-prompt: str = input('What shall propogandaAI generate: ')
-result: str = request_chat_completion(None, 'system', prompt)["choices"][0]["message"]["content"]
+prompt: str = input('What shall PropogandaAI generate: ')
+result = prompt
+# result: str = request_chat_completion(None, 'system', prompt)["choices"][0]["message"]["content"]
 
 logger.info('Generating Text on prompt')
 logger.info(f'Starting image generation based on prompt: {result}')
 
 image_generator = ImageGenerator()
-image = image_generator.generate_image(result, 512, 512)
-print(image)
+image_url = image_generator.generate_image(result, 512, 512)
+print(image_url)
+
+# Save image to file
+logger.info('Saving image to file')
+download_and_save_image(image_url, prompt)
