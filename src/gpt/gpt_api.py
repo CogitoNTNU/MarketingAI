f7a75307f32b6db3799a551830d8939a2b20bc18
Sverre Nystad <sverrenystad@gmail.com>
2023-10-09 01:29:28 +0200

    Style: format the code

--- src/gpt/gpt_api.py
+++ src/gpt/gpt_api.py
@@ -1,4 +1,3 @@
-import os
 import openai
 from openai.error import OpenAIError
 from src.config import Config
@@ -13,7 +12,7 @@ except:
     exit(1)
 
 
-def request_chat_completion(previous_message:dict, role:str = "system",message :str = ""): 
+def request_chat_completion(previous_message: dict, role: str = "system", message: str = ""): 
    
     # previous_message = get_system_prompt()
 
@@ -25,21 +24,21 @@ def request_chat_completion(previous_message:dict, role:str = "system",message :
 
         if(previous_message):
             response = openai.ChatCompletion.create(
-            model=Config().GPT_MODEL,
-            messages=[
-                previous_message,
-                {"role": role, "content": message}
-            ]
+                model = Config().GPT_MODEL,
+                messages = [
+                    previous_message,
+                    {"role": role, "content": message}
+                ]
             )
         else: 
             response = openai.ChatCompletion.create(
-            model=Config().GPT_MODEL,
-            messages=[
-                {"role": role, "content": message}, 
-            ]
+                model = Config().GPT_MODEL,
+                messages=[
+                    {"role": role, "content": message}, 
+                ]
             )
-        print(response)
         return response["choices"][0]["message"]["content"]
+    
     except OpenAIError as error:
         print(f"An error has occured while requesting chat completion.")
         print(f"The error: {str(error)}")
