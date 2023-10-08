ccfc532c913b0a8c91884d16062155f70641593e
Sverre Nystad <sverrenystad@gmail.com>
2023-10-08 14:57:47 +0200

    Feat: Integrate assembler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.assembler.image_text_assambler import assemble_image
 from src.image_generation.image_generator import ImageGenerator, download_and_save_image
 from src.gpt.gpt_api import request_chat_completion
 import logging
@@ -12,17 +13,25 @@ logging.basicConfig(filename='PropagandaAI.log',
 logger = logging.getLogger(__name__)
 
 logger.info('Starting PropagandaAI')
-prompt: str = input('What shall PropogandaAI generate: ')
-result = prompt
+user_prompt: str = input('What shall PropogandaAI generate: ')
+image_prompt = "Classic propaganda poster: Bold, primary colors with a powerful " + user_prompt
 # result: str = request_chat_completion(None, 'system', prompt)["choices"][0]["message"]["content"]
 
 logger.info('Generating Text on prompt')
-logger.info(f'Starting image generation based on prompt: {result}')
+logger.info(f'Starting image generation based on prompt: {image_prompt}')
 
 image_generator = ImageGenerator()
-image_url = image_generator.generate_image(result, 512, 512)
+image_url = image_generator.generate_image(image_prompt, 512, 512)
 print(image_url)
 
 # Save image to file
 logger.info('Saving image to file')
-download_and_save_image(image_url, prompt)
+download_and_save_image(image_url, image_prompt)
+
+# generate image text
+logger.info('Generating image text')
+template = f"This is a picture of {image_prompt}. Generate a captivating and relevant caption for a poster."
+result = "Text"
+# Assemble image
+logger.info('Assembling image')
+assemble_image(image_prompt, result, "arial.ttf", 50, (255, 155, 155), (30, 50))
