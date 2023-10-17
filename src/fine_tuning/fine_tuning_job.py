3be8189b8075232a26d1a62eb5eb3d23c7e1d969
Krisnol <krisnol@stud.ntnu.no>
2023-10-17 19:00:13 +0200

    feat: add a function for creating a job

--- src/fine_tuning/fine_tuning_job.py
+++ src/fine_tuning/fine_tuning_job.py
@@ -7,6 +7,9 @@ def upload_training_file(filepath: str) -> str:
     response = openai.File.create(file=open(filepath, "rb"), purpose="fine_tune")
     return response["id"]
 
+def create_fine_tuning_job(file_id:str, model:str):
+    openai.api_key = Config().API_KEY
+    openai.FineTuningJob.create(training_file=file_id, model=model)
 
     
 
