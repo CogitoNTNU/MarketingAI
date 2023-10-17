bc9147ffa2817f74b40db060cfaa959f7667fa04
Krisnol <krisnol@stud.ntnu.no>
2023-10-17 19:05:48 +0200

    fix: change returns

--- src/fine_tuning/fine_tuning_job.py
+++ src/fine_tuning/fine_tuning_job.py
@@ -1,11 +1,10 @@
 import openai
 from src.config import Config
 
-
 def upload_training_file(filepath: str) -> str:
     openai.api_key = Config().API_KEY
     response = openai.File.create(file=open(filepath, "rb"), purpose="fine_tune")
-    return response["id"]
+    return response
 
 def create_fine_tuning_job(file_id:str, model:str):
     openai.api_key = Config().API_KEY
