dd26c22e85164a7e50db49fd8ca3576f7d2e20b9
Olav <olavslo@stud.ntnu.no>
2023-10-31 19:08:52 +0100

    Feat: Categorize image prompts using function calling.

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from src.function_calling.no_framework_function_calling import prompt_and_parse
 from src.image_generation.image_generator import ImageGenerator, create_image_generator, download_and_save_image
 from src.gpt.text_generator import request_chat_completion
 import logging
-from PIL import Image
+
+
 
 # Set up logging
-logging.basicConfig(filename='PropagandaAI.log',
+logging.basicConfig(filename='MarketingAI.log',
                     level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-logger.info('Starting PropagandaAI')
+logger.info('Starting MarketingAI')
 user_prompt: str = input('What shall MarketingAI generate: ')
 
-answers = run_agent(user_prompt)
-logger.info(f"Finished running langchain_function_calling.py, result: {answers}")
+# Don't use framework:
+prompt_and_parse()
+
+
+# # Use framework:
+# answers = run_agent(user_prompt)
+# logger.info(f"Finished running langchain_function_calling.py, result: {answers}")
