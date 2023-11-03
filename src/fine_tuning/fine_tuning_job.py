26343cd084b2b354522df9ad38e240d24b77cfbc
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-11-03 11:40:21 +0100

    Docs: Add types and docstring to create fine tuning job

--- src/fine_tuning/fine_tuning_job.py
+++ src/fine_tuning/fine_tuning_job.py
@@ -9,10 +9,7 @@ def upload_training_file(file: str) -> str:
     response = openai.File.create(file=open(PATH_TO_DATA + file, "rb"), purpose="fine-tune")
     return response
 
-def create_fine_tuning_job(file_id:str, model:str):
+def create_fine_tuning_job(file_id: str, model: str) -> None:
+    """Creates a fine tuning job on OpenAI with the given file id and model"""
     openai.api_key = Config().API_KEY
     openai.FineTuningJob.create(training_file=file_id, model=model)
-
-if __name__ == "__main__":
-    response = upload_training_file("propaganda.jsonl")
-    print(response)
