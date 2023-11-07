88e58bc59389484ff911d3de69dc27883be1e79d
Olav <olavslo@stud.ntnu.no>
2023-11-07 18:49:40 +0100

    Feat: Add classification to image prompts.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.image_generation.image_generator import ImageGenerator, create_image_ge
 from src.gpt.text_generator import request_chat_completion
 from src.assembler.text_color import chose_color
 from src.assembler.text_size_pos import chose_font_size
+from src.function_calling.image_classifier import classify_text
 import logging
 
 from src.function_calling.image_classifier import run_agent
@@ -20,13 +21,27 @@ from src.function_calling.image_classifier import run_agent
 
 logger = logging.getLogger(__name__)
 
-# logger.info('Starting MarketingAI')
-# user_prompt: str = input('What shall MarketingAI generate: ')
+logger.info('Starting PropagandaAI')
+user_prompt: str = input('What shall PropogandaAI generate: ')
+image_prompt = "Classic propaganda poster: Bold, primary colors with a powerful " + user_prompt
+# result: str = request_chat_completion(None, 'system', prompt)["choices"][0]["message"]["content"]
 
-# Don't use framework:
-# prompt_and_parse()
+logger.info('Generating Text on prompt')
+logger.info(f'Starting image generation based on prompt: {image_prompt}')
 
+image_generator: ImageGenerator = create_image_generator('dall-e')
+image_url = image_generator.generate_image(image_prompt, 512, 512)
+logger.info(f"Image url: {image_url}")
 
-# # Use framework:
-# answers = run_agent(user_prompt)
-# logger.info(f"Finished running langchain_function_calling.py, result: {answers}")
+# Save image to file
+logger.info('Saving image to file')
+download_and_save_image(image_url, user_prompt)
+
+# generate image text
+logger.info('Generating image text')
+template = f"This is a picture of {image_prompt}. Generate a short captivating and relevant caption for a poster. The response should not contain any other information than the caption."
+result = request_chat_completion(None, 'system', template)
+logger.info(f'Generated image text: {result}')
+# Assemble image
+logger.info('Assembling image')
+assemble_image(user_prompt, result, "arial.ttf", chose_font_size(user_prompt, result, "arial.ttf", 512, 512), chose_color(user_prompt), (30, 50))
--- src/assembler/text_size_pos.py
+++ src/assembler/text_size_pos.py
@@ -1,8 +1,15 @@
 from PIL import Image, ImageFont, ImageDraw
+import os
 
-def chose_font_size(image, text, font_path, max_width, max_height):
+base_path = os.path.dirname(os.path.abspath("__main__"))
+
+def chose_font_size(image_file_name: str, text, font_path, max_width, max_height):
     font_size = 1
-    font = ImageFont.truetype(font_path, font_size)
+    font = ImageFont.truetype(base_path + "/arial.ttf", font_size)
+    image_file_path = base_path + "/data/raw_images/" + image_file_name + ".png"
+    print(image_file_path)
+
+    image = Image.open(image_file_path)
     draw = ImageDraw.Draw(image)
 
     while font.getsize(text)[0] < max_width and font.getsize(text)[1] < max_height:
